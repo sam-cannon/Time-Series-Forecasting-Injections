85fade5eda680dccbf7dcfaf6c860acb4df4429d
Sam Cannon <49358313+sam-cannon@users.noreply.github.com>
2020-02-13 12:05:34 -0800

    flask app deployed for pythonanywhere framework

--- flask_app.py
+++ flask_app.py
@@ -3,12 +3,16 @@ import numpy as np
 from flask import Flask, jsonify, request, render_template
 import pickle
 import datetime as dt
-
-# load model
-model = pickle.load(open('final_prophet_model.sav','rb'))
+import os
 
 # app
 app = Flask(__name__)
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+os.path.join(THIS_FOLDER, 'final_prophet_model.sav', 'rb')
+
+# load model
+model = pickle.load(open('/home/samcannon/mysite/final_prophet_model.sav','rb'))
 
 
 @app.route('/')
@@ -22,7 +26,7 @@ def predict():
 
     # get data from form
     date = [x for x in request.form.values()]
-    
+
     #convert data to df for prophet to predict
     prediction_df = pd.DataFrame({'ds':[dt.datetime.strptime(date, '"%Y-%m-%d"').date() for date in date]})
 
@@ -36,21 +40,21 @@ def predict():
     return render_template('index_3.html', prediction_text=f'There will be around {output} injections')
 
 @app.route('/results',methods=['POST'])
-                           
+
 def results():
 
     data = request.get_json(force=True)
-    
+
     #convert data into df
     data.update((x, [y]) for x, y in data.items())
     data_df = pd.DataFrame.from_dict(data)
-    
+
     #predictions
     result = model.predict(data_df)
 
     #output
     output = {'results': int(result.iloc[:, -1])}
-    
+
     return jsonify(output)
 
 if __name__ == '__main__':
